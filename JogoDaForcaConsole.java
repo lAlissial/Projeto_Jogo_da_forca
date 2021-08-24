4d7bfae7519e309497a43c3063763c8fa9ff6e06
lAlissial <72537882+lAlissial@users.noreply.github.com>
2021-08-24 20:15:51 -0300

    newzito

--- JogoDaForcaConsole.java
+++ JogoDaForcaConsole.java
@@ -5,10 +5,22 @@ package projeto_jodo_da_forca;
  * Aplicação console para testar a classe JogoDaForca
  *
  */
+import java.util.Locale;
 import java.util.Scanner;
 
 public class JogoDaForcaConsole {
+    /* ---------------------------- FIRULAS ---------------------------- */
+    public static final String ANSI_LIGHT_BLUE_BACKGROUND = "\u001B[104m";
+    /* ----------------------------------------------------------------- */
+
+
     public static void main(String[] args) {
+
+        /*System.out.println(ANSI_GREEN_BACKGROUND + "This text has a green background but default text!" + ANSI_RESET);
+        System.out.println(ANSI_RED + "This text has red text but a default background!" + ANSI_RESET);
+        System.out.println(ANSI_GREEN_BACKGROUND + ANSI_RED + "This text has a green background and red text!" + ANSI_RESET);
+        */
+
         Scanner teclado = new Scanner (System.in);
         JogoDaForca jogo = null;
         try {
@@ -20,29 +32,30 @@ public class JogoDaForcaConsole {
         }
         jogo.iniciar();
         String letra;
+
+        System.out.println("==================================");
+        System.out.println(ANSI_LIGHT_BLUE_BACKGROUND+"           JOGO DA FORCA          "+ANSI_RESET);
+        System.out.println("==================================");
+
         do {
             System.out.println("\nPalavra = " + jogo.getPalavra());
             System.out.println("Dica = " + jogo.getDica());
 
             System.out.print("Digite uma letra da palavra: ");
             letra = teclado.nextLine();
-            if (letra.isEmpty()){
-                System.out.println("Digite uma letra");
-            } else {
-                //tratandoo
-                try {
-                    if (jogo.adivinhou(letra)) {
-                        System.out.println("Voce acertou a letra " + letra);
-                        System.out.println("Total de acertos = " + jogo.getAcertos());
-                    } else {
-                        System.out.println("Voce errou a letra " + letra);
-                        System.out.println("Total de erros = " + jogo.getErros());
-                        System.out.println("Letras já digitadas = " + jogo.getLetrasErradas());
-                        System.out.println("Penalidade: " + jogo.getPenalidade());
-                    }
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
+            try {
+                System.out.println();
+                if (jogo.adivinhou(letra)) {
+                    System.out.println("Voce acertou a letra " + letra.toUpperCase());
+                    System.out.println("Total de acertos = " + jogo.getAcertos());
+                } else {
+                    System.out.println("Voce errou a letra " + letra);
+                    System.out.println("Total de erros = " + jogo.getErros());
+                    System.out.println("Erros já digitados = " + jogo.getLetrasErradas());
+                    System.out.println("Penalidade: " + jogo.getPenalidade());
                 }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
         while(!jogo.terminou());
@@ -50,10 +63,9 @@ public class JogoDaForcaConsole {
         System.out.println("\nPalavra = " + jogo.getPalavra());
 
         teclado.close();
-
-        //System.out.println("GAME OVER");
+        System.out.println("----------------------------------");
         System.out.println("Resultado final = "+jogo.getResultado() );
-
+        System.out.println("==================================");
 
     }
 }
